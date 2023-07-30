a64847d73a3295b23729203715830f67bf5915cd
Daryna Holikova <dgolikova0@gmail.com>
2023-07-30 12:20:22 +0100

    vimeo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+galleryItems.map(item => {
+    const markUp = `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+    gallery.insertAdjacentHTML("beforeend", markUp)
+});
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt' ,captionPosition: 'bottom', captionDelay: 250 });
+
