bf4b55b2d35430a1b62c8da1c7926231d916174e
Daryna Holikova <dgolikova0@gmail.com>
2023-07-30 19:17:37 +0100

    finilizing

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,6 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
+const LS_KEY = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
 
@@ -10,3 +11,24 @@ const iframe = document.querySelector('iframe');
     player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
+
+    const onPlay = function(data) {
+    localStorage.setItem(LS_KEY, data.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+const savedTima = localStorage.getItem(LS_KEY)
+
+player.setCurrentTime(savedTima).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,23 +5,36 @@ const form = document.querySelector(".feedback-form");
 const input = document.querySelector("input");
 const textarea = document.querySelector("textarea");
 
-form.addEventListener('input', throttle(getText, 500, { leading: false }));
-
-const formData = {};
-function getText(e) {
-    formData[e.target.name] = e.target.value;
-    // console.log(formData);
-    localStorage.setItem(LS_KEY, JSON.stringify(formData))
-};
+form.addEventListener('input', throttle(getText, 500));
 
 const savedMessage = localStorage.getItem(LS_KEY);
+const parsedMessage = JSON.parse(savedMessage);
+const formData = savedMessage ? JSON.parse(savedMessage) : {};
 
 if (savedMessage) {
-    const parsedMessage = JSON.parse(savedMessage);
     if (parsedMessage.email !== undefined) {
         input.value = parsedMessage.email;
       }
       if (parsedMessage.message !== undefined) {
         textarea.value = parsedMessage.message;
       }
-    }
+};
+
+function getText(e) {
+    const updatedFormData = { ...formData, [e.target.name]: e.target.value };
+    Object.assign(formData, updatedFormData)
+    localStorage.setItem(LS_KEY, JSON.stringify(formData))
+};
+
+form.addEventListener("click", onSudmit)
+
+function onSudmit(e) {
+    e.preventDefault()
+    if(e.target.type !== 'submit'){
+        return
+    };
+    console.log(parsedMessage)
+    form.reset();
+    localStorage.removeItem(LS_KEY)
+    
+}
