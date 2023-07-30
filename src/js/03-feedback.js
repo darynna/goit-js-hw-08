bf6f43d4d5ee2fa4d2e60228f2a76d560a105e2e
Daryna Holikova <dgolikova0@gmail.com>
2023-07-30 13:39:48 +0100

    js-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+const LS_KEY = "feedback-form-state";
 
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+
+form.addEventListener('input', throttle(getText, 500, { leading: false }));
+
+const formData = {};
+function getText(e) {
+    formData[e.target.name] = e.target.value;
+    // console.log(formData);
+    localStorage.setItem(LS_KEY, JSON.stringify(formData))
+};
+
+const savedMessage = localStorage.getItem(LS_KEY);
+
+if (savedMessage) {
+    const parsedMessage = JSON.parse(savedMessage);
+    if (parsedMessage.email !== undefined) {
+        input.value = parsedMessage.email;
+      }
+      if (parsedMessage.message !== undefined) {
+        textarea.value = parsedMessage.message;
+      }
+    }
